b44ab6b9f65c33cf56883154a654c109af3bee45
Rhys Lee <78373339+rhyslee211@users.noreply.github.com>
2024-07-22 20:35:39 -0400

    improved footer and began fixing navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,20 +14,20 @@ const Navbar = ({ HomeRef, AboutRef, ProjectsRef, ContactRef }) => {
   };
   
   return (
-    <nav className="py-1 sticky top-0 bg-slate-700 text-gray-200 flex justify-between align-middle lg:justify-start">
-      <button className="text-lg pl-1 align-middle">
+    <nav className="py-1 sticky top-0 bg-slate-700 text-gray-200 flex flex-row justify-between items-center align-middle lg:justify-evenly">
+      <button className="text-lg pl-2 lg:font-medium lg:text-base lg:hidden">
         Rhys Lee
       </button>
       <button
         data-collapse-toggle="navbar-default"
-        className="lg:hidden pr-1"
+        className="lg:hidden pr-2"
         aria-controls="navbar-default" aria-expanded="false"
       >
         <GiHamburgerMenu size={35} />
       </button>
 
       <div className="hidden w-full lg:block" id="navbar-default">
-        <ul className="font-medium flex flex-col">
+        <ul className="text-lg flex flex-col lg:flex-row lg:justify-start lg:w-1/2">
           <li className="">
             <button className="" onClick={() => scrollToSection(HomeRef)}>
               Home
